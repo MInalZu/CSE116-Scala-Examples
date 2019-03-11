beeca8b075a99305003a85122f99b8fdc1a240dc
jessehartloff <hartloff@buffalo.edu>
2019-03-11 10:36:22 -0400

    update method names

--- src/sorting/SelectionSort.scala
+++ src/sorting/SelectionSort.scala
@@ -17,7 +17,7 @@ object SelectionSort {
       for (j <- i until data.size) {
         val currentValue = data.apply(j)
 
-        // make decisions based on the given comparator (this function can be thought of as a less than operator
+        // make decisions based on the given comparator (this function can be thought of as a less than operator)
         if (comparator(currentValue, minFound)) {
           minFound = currentValue
           minIndex = j
@@ -33,15 +33,14 @@ object SelectionSort {
     data
   }
 
-  def mySorting(): Unit = {
+  def intSortingExample(): Unit = {
     val numbers = List(5, -23, -8, 7, -4, 10)
     val numbersSorted = intSelectionSort(numbers, (a: Int, b: Int) => a > b)
     println(numbersSorted)
   }
 
 
-
-  def myGenericSorting(): Unit = {
+  def genericSortingExample(): Unit = {
     val animals: List[Animal] = List(new Cat("morris"), new Dog("Finn"), new Dog("Snoopy"), new Cat("Garfield"))
     val animalsSorted = selectionSort(animals, Animal.compareAnimals)
     println(animalsSorted)
@@ -67,15 +66,15 @@ object SelectionSort {
   }
 
 
-  def largeExample(): Unit ={
-    val numbers: List[Double] = (for(_ <- 0 to 2000) yield Math.random()).toList
+  def largeExample(): Unit = {
+    val numbers: List[Double] = (for (_ <- 0 to 2000) yield Math.random()).toList
     selectionSort(numbers, (x: Double, y: Double) => x < y)
     println(numbers)
   }
 
   def main(args: Array[String]): Unit = {
-    mySorting()
-    myGenericSorting()
+    intSortingExample()
+    genericSortingExample()
     largeExample()
   }
 
