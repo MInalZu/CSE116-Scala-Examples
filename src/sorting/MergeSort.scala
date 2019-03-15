ff07283ab8c2f8436583880b9d4a64ea10abee91
jessehartloff <hartloff@buffalo.edu>
2019-03-15 03:09:16 -0400

    add CustomSorting for an example of a method that returned a function

--- src/sorting/MergeSort.scala
+++ src/sorting/MergeSort.scala
@@ -4,58 +4,58 @@ import scala.collection.mutable.ListBuffer
 
 object MergeSort {
 
-def mergeSort[T](inputData: List[T], comparator: (T, T) => Boolean): List[T] = {
-  if (inputData.length < 2) {
-    inputData
-  } else {
-    val mid: Int = inputData.length / 2
-    val (left, right) = inputData.splitAt(mid)
-    val leftSorted = mergeSort(left, comparator)
-    val rightSorted = mergeSort(right, comparator)
-
-    foreshadowMerge(leftSorted, rightSorted, comparator)
-  }
-}
-
-  // This method is given as an example, though it is very inefficient
-def merge[T](left: List[T], right: List[T], comparator: (T, T) => Boolean): List[T] = {
-  var leftPointer = 0
-  var rightPointer = 0
-
-  var sortedList: List[T] = List()
-
-  while (leftPointer < left.length && rightPointer < right.length) {
-    if (comparator(left.apply(leftPointer), right.apply(rightPointer))) {
-      sortedList = sortedList :+ left.apply(leftPointer)
-      leftPointer += 1
+  def mergeSort[T](inputData: List[T], comparator: (T, T) => Boolean): List[T] = {
+    if (inputData.length < 2) {
+      inputData
     } else {
-      sortedList = sortedList :+ right.apply(rightPointer)
-      rightPointer += 1
+      val mid: Int = inputData.length / 2
+      val (left, right) = inputData.splitAt(mid)
+      val leftSorted = mergeSort(left, comparator)
+      val rightSorted = mergeSort(right, comparator)
+
+      foreshadowMerge(leftSorted, rightSorted, comparator)
     }
   }
 
-  while (leftPointer < left.length) {
-    sortedList = sortedList :+ left.apply(leftPointer)
-    leftPointer += 1
-  }
-  while (rightPointer < right.length) {
-    sortedList = sortedList :+ right.apply(rightPointer)
-    rightPointer += 1
-  }
+  // This method is given as an example, though it is very inefficient
+  def merge[T](left: List[T], right: List[T], comparator: (T, T) => Boolean): List[T] = {
+    var leftPointer = 0
+    var rightPointer = 0
 
-  sortedList
-}
+    var sortedList: List[T] = List()
+
+    while (leftPointer < left.length && rightPointer < right.length) {
+      if (comparator(left.apply(leftPointer), right.apply(rightPointer))) {
+        sortedList = sortedList :+ left.apply(leftPointer)
+        leftPointer += 1
+      } else {
+        sortedList = sortedList :+ right.apply(rightPointer)
+        rightPointer += 1
+      }
+    }
+
+    while (leftPointer < left.length) {
+      sortedList = sortedList :+ left.apply(leftPointer)
+      leftPointer += 1
+    }
+    while (rightPointer < right.length) {
+      sortedList = sortedList :+ right.apply(rightPointer)
+      rightPointer += 1
+    }
+
+    sortedList
+  }
 
   // Same functionality as merge, but uses linked lists much more efficiently
   def foreshadowMerge[T](left: List[T], right: List[T], comparator: (T, T) => Boolean): List[T] = {
 
-    if(left.length == 1 && right.length == 1){
-      if(comparator(left.head, right.head)){
+    if (left.length == 1 && right.length == 1) {
+      if (comparator(left.head, right.head)) {
         List(left.head, right.head)
-      }else{
+      } else {
         List(right.head, left.head)
       }
-    }else {
+    } else {
 
       val leftIter = left.iterator
       val rightIter = right.iterator
@@ -70,11 +70,11 @@ def merge[T](left: List[T], right: List[T], comparator: (T, T) => Boolean): List
 
       while (leftIter.hasNext || rightIter.hasNext) {
 
-        if(leftUpdated){
+        if (leftUpdated) {
           leftVal = leftIter.next
           leftUpdated = false
         }
-        if(rightUpdated){
+        if (rightUpdated) {
           rightVal = rightIter.next
           rightUpdated = false
         }
@@ -111,11 +111,11 @@ def merge[T](left: List[T], right: List[T], comparator: (T, T) => Boolean): List
 
     // switch merge sort with Scala's builtin algorithm for timing comparisons (The builtin should always be faster!)
     // Use the foreshadowMerge method for comparable times
-//    val sortedNumbers = numbers.sortWith((x: Double, y: Double) => x.abs > y.abs)
+    //    val sortedNumbers = numbers.sortWith((x: Double, y: Double) => x.abs > y.abs)
 
     val totalTime = (System.nanoTime() - start) / 1000000.0
 
-//    sortedNumbers.foreach(println)
+    //    sortedNumbers.foreach(println)
     println(f"\nSorting took $totalTime%1.2f ms")
   }
 
