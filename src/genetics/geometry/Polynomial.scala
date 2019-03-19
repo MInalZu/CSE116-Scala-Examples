b3d0b3b402271112920aa192ea91cf60689eef70
jessehartloff <hartloff@buffalo.edu>
2019-03-19 12:00:49 -0400

    note on superscript markup

--- src/genetics/geometry/Polynomial.scala
+++ src/genetics/geometry/Polynomial.scala
@@ -5,6 +5,8 @@ package genetics.geometry
   *
   * Ex. new Polynomial(List(5, -2, 1)) represents x^2^ - 2x + 5
   *
+  * Note: The extra carrot is part of scaladoc syntax. x^2^ == "x squared"
+  *
   */
 class Polynomial(val coefficients: List[Double]) {
 
