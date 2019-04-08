2db6e3bedab2e9d269c558696b7551776395d4df
jessehartloff <hartloff@buffalo.edu>
2019-04-08 11:13:19 -0400

    First concurrency example

---
+++ src/concurrency/ConcurrentCounters.scala
@@ -0,0 +1,37 @@
+package concurrency
+
+import akka.actor._
+
+
+case object Start
+
+
+class Counter(name: String) extends Actor {
+
+  def countDown(n: Int): Unit = {
+    if (n >= 0) {
+      println(this.name + " - " + n)
+      countDown(n - 1)
+    }else{
+      println(this.name + " finished")
+    }
+  }
+
+  def receive: Receive = {
+    case Start => countDown(20)
+  }
+
+}
+
+
+object PingPongTest extends App {
+  val system = ActorSystem("CountingSystem")
+
+  val one = system.actorOf(Props(classOf[Counter], "1"))
+  val two = system.actorOf(Props(classOf[Counter], "2"))
+  val three = system.actorOf(Props(classOf[Counter], "3"))
+
+  one ! Start
+  two ! Start
+  three ! Start
+}
